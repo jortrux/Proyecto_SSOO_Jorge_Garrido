809d0fc9f194a47cf4cff83a373ebb31e0922345
jortrux <107060335+jortrux@users.noreply.github.com>
2022-10-31 18:23:25 +0100

    Update practica.c
    correcciones y lectura de fichero

--- practica.c
+++ practica.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define NUM_FILAS 8
+#define TAM_LINEA 16
+
 typedef struct {
 	unsigned char ETQ;
 	unsigned char Data[TAM_LINEA];
@@ -10,10 +13,19 @@ typedef struct {
 
 void limpiaCACHE(T_LINEA_CACHE tb1[NUM_FILAS]);
 void VuelcaCACHE(T_LINEA_CACHE *tbl);
-void parsearAddr(unsigned int addr; int *ETQ, int *palabra, int *linea, int *bloque);
+void parsearAddr(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque);
 void TratarFallo(T_LINEA_CACHE *tb1, char *MRAM, int ETQ, int linea, int bloque);
 
 void main(int argc, char** argv){
-
-
+	int glovaltime=0;
+	int numfallos=0;
+	FILE *fd;
+	fd=fopen("CONTENTS_RAM.bin","r");
+	if(fd==NULL){
+		printf("Error, el archivo no existe");
+	}else{
+		printf("el archivo existe");
+	}
+	fclose(fd);
 }
+
