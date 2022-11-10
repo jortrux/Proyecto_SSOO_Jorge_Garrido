8bb2539a2fcdf0fce0377d6491e12600bca157b7
jortrux <107060335+jortrux@users.noreply.github.com>
2022-11-10 16:00:02 +0100

    Add files via upload
    inclusion del string Sim_Ram y lectura del fichero contents_ram.bin

--- practica.c
+++ practica.c
@@ -11,21 +11,47 @@ typedef struct {
 	unsigned char Data[TAM_LINEA];
 } T_LINEA_CACHE;
 
-void limpiaCACHE(T_LINEA_CACHE tb1[NUM_FILAS]);
+void limpiaCACHE(T_LINEA_CACHE tbl[NUM_FILAS]);
 void VuelcaCACHE(T_LINEA_CACHE *tbl);
 void parsearAddr(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque);
-void TratarFallo(T_LINEA_CACHE *tb1, char *MRAM, int ETQ, int linea, int bloque);
+void TratarFallo(T_LINEA_CACHE *tbl, char *MRAM, int ETQ, int linea, int bloque);
 
 void main(int argc, char** argv){
 	int glovaltime=0;
 	int numfallos=0;
+	unsigned char Simul_Ram[4096];
+	int i=0;
 	FILE *fd;
+
+
 	fd=fopen("CONTENTS_RAM.bin","r");
+        printf("%p",fd);
+        if(fd==NULL){
+                printf("Error, el archivo no existe");
+        }else{
+                printf("el archivo existe");
+
+
+                while(!feof(fd)){
+                       	Simul_Ram[i]= fgetc(fd);
+			i++;
+        	}
+
+                fclose(fd);
+        }
+
+
+	printf("%s",Simul_Ram);
+
+
+	fd=fopen("accesos_memoria.txt","r");
+	printf("%p",fd);
 	if(fd==NULL){
 		printf("Error, el archivo no existe");
 	}else{
-		printf("el archivo existe");
-	}
-	fclose(fd);
-}
 
+		printf("el archivo existe");
+
+		fclose(fd);
+	}
+}
